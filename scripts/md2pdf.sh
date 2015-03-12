6be6770d8e2658f49d3b409e8cb3e35fc6b114a2
Stephen Turner <vustephen@gmail.com>
2015-03-12 16:01:55 -0400

    add ability to pass pandoc options to md2pdf; no longer need md2pdf-toc because can just specify opiton

--- scripts/md2pdf.sh
+++ scripts/md2pdf.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
 
-# Uses pandoc to convert all the markdown documents in the CWD to PDF.
+# Use pandoc to convert markdown files to PDF
 
-if [ "$#" -eq 0 ]; then
-	echo "Usage: $0 <file1.md> [file2.md file3.md ...]"
-	exit 1
+# Set default pandoc options
+PANDOCOPTS=""
+
+# parse the options
+while getopts 'o:' opt ; do
+  case $opt in
+    o) PANDOCOPTS=$OPTARG ;;
+  esac
+done
+
+# skip over the processed options
+shift $((OPTIND-1)) 
+
+# check for mandatory positional parameters
+if [ $# -eq 0 ]; then
+  echo "Usage: $0 [options] <file1.md> [file2.md file3.md ...]"
+  echo "Options: -o '--options --passed --to --pandoc'"
+  exit 1
 fi
+
+# Create a pdf file for each markdown file
 for md in "$@"
 do
-	CMD="pandoc -s -V geometry:margin=1in -V documentclass:article -V fontsize=12pt $md -o $md.pdf"
-	echo $CMD
-	$CMD
+  CMD="pandoc $PANDOCOPTS -s -V geometry:margin=1in -V documentclass:article -V fontsize=12pt $md -o $md.pdf"
+  echo $CMD
+  $CMD
 done
