a56c3acf9640ec212d148601e2df87e438c47a06
Stephen Turner <vustephen@gmail.com>
2015-04-07 11:10:35 -0400

    dont destroy index.html, just write to stdout

--- scripts/makeindex.sh
+++ scripts/makeindex.sh
@@ -2,8 +2,6 @@
 
 # Writes an index.html file for all the files in the CWD, recursively
 
-out="index.html"
-
-echo '<html><body>' > $out
-find . -type f | grep -v "^\.$" | grep -v "index.html" | sed 's/^\.\///' | sed 's/^.*/<a href="&">&<\/a><br\/>/' >> $out
-echo '</body></html>' >> $out
+echo '<html><body>'
+find . -type f | grep -v "^\.$" | grep -v "index.html" | sed 's/^\.\///' | sed 's/^.*/<a href="&">&<\/a><br\/>/'
+echo '</body></html>'
