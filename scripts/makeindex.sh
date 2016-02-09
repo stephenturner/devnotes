ca007214e01ea766058596517df7700d5db79214
Stephen Turner <vustephen@gmail.com>
2016-02-09 15:44:33 -0500

    minor change

--- scripts/makeindex.sh
+++ scripts/makeindex.sh
@@ -3,5 +3,5 @@
 # Writes an index.html file for all the files in the CWD, recursively
 
 echo '<html><body>'
-find . -type f | grep -v "^\.$" | grep -v "index.html" | sed 's/^\.\///' | sed 's/^.*/<a href="&">&<\/a><br\/>/'
+find . -type f -not -path '*/\.*' -not -name "index.html" | sed 's/^\.\///' | sed 's/^.*/<a href="&">&<\/a><br\/>/'
 echo '</body></html>'
