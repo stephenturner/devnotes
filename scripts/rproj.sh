96f70031925f1865191c2301186da3e0e6ae779c
Stephen Turner <vustephen@gmail.com>
2016-07-12 16:04:31 -0400

    create new rproj script

---
+++ scripts/rproj.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Writes a standard Rproj file to workingDirName.Rproj
+
+PROJFILE="$(basename $(pwd)).Rproj"
+
+cat > $PROJFILE << EOF
+Version: 1.0
+
+RestoreWorkspace: Default
+SaveWorkspace: Default
+AlwaysSaveHistory: Default
+
+EnableCodeIndexing: Yes
+UseSpacesForTab: Yes
+NumSpacesForTab: 2
+Encoding: UTF-8
+
+RnwWeave: Sweave
+LaTeX: pdfLaTeX
+
+AutoAppendNewline: Yes
+StripTrailingWhitespace: Yes
+EOF
+
+echo "Created new project file: $PROJFILE"
