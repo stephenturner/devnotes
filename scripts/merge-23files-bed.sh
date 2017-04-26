16cd06467d407430067d85a6944ac5b3af375fab
Stephen Turner <vustephen@gmail.com>
2017-04-26 15:38:39 -0400

    update ghurl and merge-23files-bed

--- scripts/merge-23files-bed.sh
+++ scripts/merge-23files-bed.sh
@@ -3,15 +3,18 @@
 # Takes a bunch of 23andMe formatted files and merges them into a single plink bed file.
 # Usage: merge-23files-bed.sh *.23file.txt | bash
 
-i=1
+echo "#!/bin/env bash"
+echo
+echo "# Create individual bed files:"
+i=0
 for F in "$@"; do
-  CMD="plink --23file $F $F 1 0 --snps-only no-DI --out $F.23 --make-bed"
+  CMD="plink --23file $F $F 1 0 --snps-only just-acgt --out $F.23 --make-bed"
   echo $CMD
-  if [ "$i" -eq 1 ]; then 
+  if [ "$i" -eq 0 ]; then 
     FIRST="$F.23"
     echo "echo -n > tmp.mergelist.txt"
   fi
-  if [ "$i" -gt 1 ]; then
+  if [ "$i" -gt 0 ]; then
     echo "echo $F.23 >> tmp.mergelist.txt"
   fi
   ((i+=1))
@@ -19,7 +22,7 @@ for F in "$@"; do
 done
 
 echo
+echo "# merge **$i** files:"
+echo "plink --bfile $FIRST --merge-list tmp.mergelist.txt --out merge"
 
-echo "plink --bfile $FIRST --merge-list tmp.mergelist.txt --out merged"
-
-echo "rm -f tmp.mergelist.txt*"
+# echo "rm -f tmp.mergelist.txt*"
