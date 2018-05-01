970d65dbbbb357fa193b515841cb17a84dfbe267
Stephen Turner <vustephen@gmail.com>
2018-05-01 08:58:48 -0400

    update server admin

--- new-linux-server-setup/00-administration.sh
+++ new-linux-server-setup/00-administration.sh
@@ -1,7 +1,24 @@
 # Create a new group:
-sudo groupadd -g 18697 groupname
+sudo groupadd -g 18697 uvabx
 
 # Add users to that group:
-sudo usermod -a -G groupname user1
-sudo usermod -a -G groupname user2
-sudo usermod -a -G groupname user3
+sudo usermod -a -G uvabx sdt5z
+sudo usermod -a -G uvabx afk2s
+
+# Mount project dir
+sudo mkdir -p /project/uvabx
+sudo vim /etc/fstab # add line see email
+sudo mount /project/uvabx
+
+# Home dirs
+sudo mkdir /tiny/sdt5z
+sudo mkdir /tiny/afk2s
+
+
+# project storage
+mkdir /project/g_bme-janeslab
+mount /project/g_bme-janeslab
+useradd -u 281689 -g 15685 kaj5f
+groupadd -g  15685 g_bme-janeslab
+usermod -a -G  g_bme-janeslab sdt5z
+usermod -a -G  g_bme-janeslab afk2s
